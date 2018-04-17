ba94433aed2964193b14341931cc48b9ea76ee8b
anneteja <anneteja49@gmail.com>
2018-04-17 19:22:10 +0000

    added basic Apache cookbook

--- .chef/knife.rb
+++ .chef/knife.rb
@@ -1,9 +1,9 @@
-# See https://docs.getchef.com/config_rb_knife.html for more information on knife configuration options
+# See http://docs.chef.io/config_rb_knife.html for more information on knife configuration options
 
 current_dir = File.dirname(__FILE__)
 log_level                :info
 log_location             STDOUT
-node_name                "teja"
-client_key               "#{current_dir}/teja.pem"
-chef_server_url          "https://anneteja2.mylabserver.com/organizations/sagar"
+node_name                "anneteja"
+client_key               "#{current_dir}/anneteja.pem"
+chef_server_url          "https://api.chef.io/organizations/anne49"
 cookbook_path            ["#{current_dir}/../cookbooks"]
